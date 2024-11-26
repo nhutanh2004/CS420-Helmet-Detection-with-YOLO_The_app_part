c2e2e733a90866748f5e3c51305de95e5aa48907
gbaonr <gbaon.r@gmail.com>
2024-11-26 21:00:49 +0700

    add deepsort

--- app.py
+++ app.py
@@ -13,11 +13,18 @@ import numpy as np
 from werkzeug.utils import secure_filename
 import subprocess
 import sys
+from deep_sort_realtime.deep_sort_realtime.deepsort_tracker import DeepSort
+from deep_sort_realtime import deep_sort_realtime
+
+print("\n\n")
+print(deep_sort_realtime.__file__)
+print("\n\n")
 
 sys.path.append("backend/source")
 
 from backend.source.test import run_on_frame, create_models
 from backend.source.utils.visualize import plot_bbox
+
 
 app = Flask(__name__)
 
@@ -38,6 +45,20 @@ def get_model_weights():
         for model_name in os.listdir(weights_dir)
         if model_name.endswith(".pt")
     ]
+
+
+# convert ltwh to xyxy
+def ltwh2xyxy(box):
+    x1, y1, w, h = box
+    x2, y2 = x1 + w, y1 + h
+    return [x1, y1, x2, y2]
+
+
+# convert xyxy to ltwh
+def xyxy2ltwh(box):
+    x1, y1, x2, y2 = box
+    w, h = x2 - x1, y2 - y1
+    return [x1, y1, w, h]
 
 
 # Create models using the provided weights
@@ -118,7 +139,15 @@ def process_video():
 
     frame_counter = 0  # Initialize frame counter
 
-    b, s, l = [], [], []  # Initialize lists for bounding boxes, labels, and scores
+    # Init Deep SORT tracker
+    print("Initializing Deep SORT tracker...\n\n")
+    tracker = DeepSort(
+        max_age=30,
+        n_init=1,
+        max_cosine_distance=0.2,
+        max_iou_distance=0.8,
+        # gating_only_position=True,
+    )
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -136,7 +165,7 @@ def process_video():
         # Convert the frame to RGB
         img_array = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Initialize lists for bounding boxes, labels, and scores
+        # Run the models on the frame (bbox is in xyxy format)
         boxes, labels, scores = run_on_frame(
             models, img_array, "1_1.jpg", iou_thr, skip_box_thr, p
         )
@@ -146,19 +175,52 @@ def process_video():
 
         # Truncate virtual boxes and denormalize real boxes
         truncated_boxes, truncated_labels, truncated_scores = [], [], []
+        tracker_boxes = []
         for i in range(len(labels)):
             if scores[i] > 0.001:
                 boxes[i][0] = int(boxes[i][0] * target_width)
                 boxes[i][1] = int(boxes[i][1] * target_height)
                 boxes[i][2] = int(boxes[i][2] * target_width)
                 boxes[i][3] = int(boxes[i][3] * target_height)
+
                 truncated_boxes.append(boxes[i])
                 truncated_labels.append(labels[i])
                 truncated_scores.append(scores[i])
 
+                boxes[i] = xyxy2ltwh(boxes[i])
+                tracker_boxes.append((boxes[i], scores[i], labels[i]))
+
+        # Update tracker
+        tracks = tracker.update_tracks(tracker_boxes, frame=frame)
+        deep_sort_boxes = []
+        deep_sort_labels = []
+        deep_sort_scores = []
+
+        for track in tracks:
+            # if not track.is_confirmed() :
+            #     continue
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue
+            track_id = track.track_id
+            ltrb_box = track.to_ltrb()
+
+            deep_sort_boxes.append(ltrb_box)
+            deep_sort_labels.append(track.get_det_class())
+            deep_sort_scores.append(track.get_det_conf())
+
+        deep_sort_scores = [
+            float(score) if score is not None else 0.0 for score in deep_sort_scores
+        ]
+
+        print(
+            f"deep_sort_boxes: {len(deep_sort_boxes)}, {len(deep_sort_labels)}, {len(deep_sort_scores)}"
+        )
+        print(f"deep_sort_scores: {deep_sort_scores}")
+        print(f"deep_sort_labels: {deep_sort_labels}\n\n")
+
         # Draw bounding boxes on the frame
         drew_frame = plot_bbox(
-            frame, truncated_boxes, truncated_labels, truncated_scores
+            frame, deep_sort_boxes, deep_sort_labels, deep_sort_scores
         )
 
         # Write the processed frame to the output video
@@ -170,9 +232,7 @@ def process_video():
 
     # Compress the video using ffmpeg command line and capture stderr
     counter = 0
-    compressed_output_path = os.path.join(
-        "static", f"{filename.split(".")[0]}.mp4"
-    )
+    compressed_output_path = os.path.join("static", f"{filename.split(".")[0]}.mp4")
     while os.path.exists(compressed_output_path):
         counter += 1
         compressed_output_path = os.path.join(
